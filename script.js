75a5d8339d3955d9d774e7ccbb6e692b7ee08161
Daniel Wagner-Hall <dawagner@gmail.com>
2020-04-16 23:30:22 +0100

    level 10: Ugly but functional

--- script.js
+++ script.js
@@ -4,9 +4,45 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function zeroPad(number) {
+  if (number < 10) {
+    return `0${number}`;
+  }
+  return `${number}`;
+}
+
+function episodeCode(episode) {
+  return `S${zeroPad(episode.season)}E${zeroPad(episode.number)}`;
+}
+
 function makePageForEpisodes(episodeList) {
   const displayElem = document.getElementById("episodesDisplay");
-  displayElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((episode, index) => {
+    const episodeContainer = document.createElement("div");
+    episodeContainer.className = "episode-container"
+
+    const header = document.createElement("div");
+    header.innerText = `${episodeCode(episode)} - ${episode.name}`;
+    episodeContainer.appendChild(header);
+
+    const image = document.createElement("img");
+    image.src = episode.image.medium;
+    episodeContainer.appendChild(image);
+
+    const description = document.createElement("div");
+    episodeContainer.appendChild(description);
+    // BOO! Insecure!
+    description.outerHTML = episode.summary;
+
+    episodeContainer.appendChild(description);
+    displayElem.appendChild(episodeContainer);
+
+    if (index % 3 === 2) {
+      const divider = document.createElement("div");
+      divider.style.clear = "both";
+      displayElem.appendChild(divider);
+    }
+  });
 }
 
 window.onload = setup;
